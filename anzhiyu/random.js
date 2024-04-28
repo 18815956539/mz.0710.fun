1d6bace27a101d33bc6d7e28f3eeba30e7d40529
18815956539 <chenjianan20110607@outlook.com>
2024-04-28 13:27:59 +0800

    Site updated: 2024-04-28 13:27:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/27/bug修复完毕/"];function toRandomPost(){
+var posts=["2024/04/27/bug修复完毕/","2024/04/27/“音乐馆和主页的音乐”/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
